f65e3cef6260fcd9410d33d089cbe71cac411646
Gaia Ghidoni <gaia.ghid@gmail.com>
2025-02-26 11:11:41 +0100

    tolte alcune stampe

--- Tiling.py
+++ Tiling.py
@@ -2,6 +2,7 @@ import os
 import os.path
 import argparse
 import sys
+import time
 import tkinter as tk
 import numpy as np
 from tkinter import filedialog
@@ -111,43 +112,42 @@ class Tiling:
 
     def start_search(self):
         # TODO togli tutte le stampe
-        # TODO modifica questione controllo/overflow bande se selezione sul bordo
         og_img = self.image.convert('RGB')
         og_img = np.array(og_img)
         h_diff_values = []
         v_diff_values = []
+        txt_path = os.path.join(OUT_DIR, "search_info.txt")
+        f = open(txt_path, "w")
 
         # ricerca orizzontale
-        print("h border search area (size) " + str(self.bo))
-        print("width " + str(self.width) + " border search area ratio " + str(self.search_ratio))
+        # print("h border search area (size) " + str(self.bo))
+        # print("width " + str(self.width) + " border search area ratio " + str(self.search_ratio))
         step = 0
         min_diff_right = (1, 0)  # tuple containing (min difference, step)
 
         # bordo sinistro partendo dalla coordinata 0 della selezione dell'utenete
         left_border = self.left_border.mat
 
+        f.write("#left" + str(self.left_border.start) + " " + str(self.left_border.end) + "\n")
+
+        start_time = time.time()
         #+++++++++++ fissato a sx, sposto il bordo di dx
         while step < 2 * self.bo:  # ricerca  nell'area tra -bo e +bo
-            print("############### STEP " + str(step))
 
             r_start = (self.right_border.start[0] + step, self.right_border.start[1])
             r_end = (self.right_border.end[0] + step, self.right_border.end[1])
 
             right_border = og_img[r_start[1]: r_end[1], r_start[0]: r_end[0], :]
 
-            if step == 0:
-                print("user selection coord " + str(self.start) + " " + str(self.end))
-                print("orig. selected wxh = " + str(self.width) + "x" + str(self.height))
-                print("left border shape = " + str(left_border.shape))
-                print("right border shape = " + str(right_border.shape))
 
             diff = self.normalize(right_border) - self.normalize(left_border)
             m_norm = sum(sum(sum(abs(diff)))) / right_border.size  # Manhattan norm
             if m_norm < min_diff_right[0]:
                 min_diff_right = (m_norm, step)
             h_diff_values.append(m_norm)
-            print("diff (M norm): " + str(m_norm))
             step += 1
+            f.write(str(step)+" " + str(r_start) + " " + str(r_end) + " " + str(m_norm) + "\n")
+
 
         print("Minimun (h) distanze between borders find at step " + str(min_diff_right[1]) + ": " + str(
             min_diff_right[0]))
@@ -160,33 +160,32 @@ class Tiling:
         # bordo top partendo dalla coordinata 0 della selezione dell'utenete
         top_border = self.top_border.mat
 
+        f.write("\n\n#top" + str(self.top_border.start) + " " + str(self.top_border.end) + "\n")
         # +++++++++++ fissato top, sposto il bordo bottom
         while step < 2 * self.bv:  # ricerca  nell'area tra -bo e +bo
-            print("############### v STEP " + str(step))
 
             b_start = (self.bottom_border.start[0], self.bottom_border.start[1] + step)
             b_end = (self.bottom_border.end[0], self.bottom_border.end[1] + step)
 
             bottom_border = og_img[b_start[1]: b_end[1], b_start[0]: b_end[0], :]
 
-            if step == 0:
-                print("user selection coord " + str(self.start) + " " + str(self.end))
-                print("orig. selected wxh = " + str(self.width) + "x" + str(self.height))
-                print("right border shape = " + str(top_border.shape))
-                print("bottom border shape = " + str(bottom_border.shape))
-
             diff = self.normalize(bottom_border) - self.normalize(top_border)
             m_norm = sum(sum(sum(abs(diff)))) / bottom_border.size  # Manhattan norm
             if m_norm < min_diff_bottom[0]:
                 min_diff_bottom = (m_norm, step)
             v_diff_values.append(m_norm)
-            print("diff (M norm): " + str(m_norm))
             step += 1
+            f.write(str(step)+" " + str(b_start) + " " + str(b_end) + " " + str(m_norm) + "\n")
+
+        end_time = time.time()
 
         print("Minimun (v) distanze between borders find at step " + str(min_diff_bottom[1]) + ": " + str(
             min_diff_bottom[0]))
 
+        f.write("\n\n tot time: " + str(end_time-start_time) + " sec\n")
         #++++++++++++++++++++++++++++++++++++++++++++++++++
+        # chiusura file dati
+        f.close()
 
         #aggiornamento valori bordo dx
         self.right_border.update(self.right_border.start[0] + min_diff_right[1], self.right_border.start[1],
@@ -215,7 +214,6 @@ class Tiling:
 
         imgc = Image.fromarray(np.array(self.crop(self.image, self.start, self.end).convert('RGB')), mode='RGB')
         # print("selection crop "+str(self.selection.width)+" x "+str(self.selection.height))
-        print("crop size - "+str(imgc.size))
         self.save_img(imgc, 'user_crop.png')
 
         self.save_img(self.tiled, file_name="tiled.png")
@@ -274,9 +272,6 @@ class Tiling:
         # data
         end_y = len(values) - range_limit
         points = range(-range_limit, end_y)
-        print("x: " + str(len(values)))
-        print("y: " + str(len(points)))
-        print(points)
 
         if direction == 0:
             y = points
