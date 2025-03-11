bedab22a5c276c54656d956676c50dcaa60df066
Gaia Ghidoni <gaia.ghid@gmail.com>
2025-03-11 21:10:25 +0100

    una versione di outside che funziona a metà

--- ShearRectangle.py
+++ ShearRectangle.py
@@ -36,6 +36,7 @@ class ShearRectangle:
 
     def __init__(self, canvas, container, width, height, coords=None):
         # Create attributes needed to display selection.
+        self.draft = None
         self.moving_start = None
         self.canvas = canvas
         self.select_opts1 = SELECT_OPTS
@@ -76,6 +77,11 @@ class ShearRectangle:
                      self.canvas.create_line(imax_x, imax_y, imin_x, imax_y, **select_opts2, tags=("line",)),  #c-d
                      self.canvas.create_line(imin_x, imax_y, imin_x, imin_y, **select_opts2, tags=("line",)),  #d-a
                      )
+        self.outside = (
+            self.canvas.create_polygon(0, 0, 0, self.height, self.width, self.height, c[0], c[1], d[0], d[1], a[0],
+                                       a[1], **select_opts1),
+            self.canvas.create_polygon(0, 0, self.width, 0, self.width, self.height, c[0], c[1], b[0], b[1], a[0],
+                                       a[1], **select_opts1))
 
         if coords is not None:
             self.update(self.start, self.end)
@@ -93,17 +99,18 @@ class ShearRectangle:
         :return: TODO
         """
         print("click")
+        # TODO controllo nel caso si faccia shift + drag ma senza selezione iniziale
         self.moving_start = (event.x, event.y)
-        draft = tk.Canvas(self.canvas)  # if w is deleted, the draft is deleted
-        draft.delete("all")  # if you use the fake canvas for other uses
+        self.draft = tk.Canvas(self.canvas)  # if w is deleted, the draft is deleted
+        self.draft.delete("all")  # if you use the fake canvas for other uses
         concerned = self.canvas.find_withtag("line")  # what you want
         for obj in concerned:
             config = {opt: self.canvas.itemcget(obj, opt) for opt in self.canvas.itemconfig(obj)}
             config["tags"] = str(obj)  # I can retrieve the ID in "w" later with this trick
-            draft.create_line(*self.canvas.coords(obj), **config)
+            self.draft.create_line(*self.canvas.coords(obj), **config)
 
-        element = draft.find_closest(draft.canvasx(event.x), draft.canvasy(event.y))
-        coords = draft.coords(element)
+        element = self.draft.find_closest(self.draft.canvasx(event.x), self.draft.canvasy(event.y))
+        coords = self.draft.coords(element)
         p1 = np.asarray((coords[0], coords[1]))
         p2 = np.asarray((coords[2], coords[3]))
         dist = norm(np.cross(p2 - p1, p1 - np.asarray((event.x, event.y)))) / norm(p2 - p1)
@@ -112,6 +119,7 @@ class ShearRectangle:
         print("dist: " + str(dist))
         if dist < MIN_DIST:
             self.selected = element[0]
+            self.original = self.rect
 
     def move_line(self, event):
         pan = (self.canvas.canvasx(0), self.canvas.canvasy(0))
@@ -124,21 +132,20 @@ class ShearRectangle:
             element = self.rect[self.selected - 1]
             # calculate distance moved from last position
             dx, dy = event.x - self.moving_start[0], event.y - self.moving_start[1]
-            #TODO controllo qui
             current = self.canvas.coords(element)
 
             miny = min(current[1], current[3])
             minx = min(current[0], current[2])
             maxy = max(current[1], current[3])
             maxx = max(current[0], current[2])
-
-            if dx < 0 and minx+dx < omin_x:
+            # do not exceed og image coordinates:
+            if dx < 0 and minx + dx < omin_x:
                 dx = minx - omin_x
-            if dy < 0 and miny+dy < omin_y:
+            if dy < 0 and miny + dy < omin_y:
                 dy = miny - omin_y
-            if dx > 0 and maxx+dx > omax_x:
+            if dx > 0 and maxx + dx > omax_x:
                 dx = omax_x - maxx
-            if dy > 0 and maxy+dy > omax_y:
+            if dy > 0 and maxy + dy > omax_y:
                 dy = omax_y - maxy
 
             # move the selected item
@@ -156,7 +163,62 @@ class ShearRectangle:
             self.canvas.coords(index1, x1, y1, ax, ay)
             self.canvas.coords(index2, bx, by, x2, y2)
 
+            #TODO togli questo pezzo di codice duplicato
+            self.draft = tk.Canvas(self.canvas)  # if w is deleted, the draft is deleted
+            self.draft.delete("all")  # if you use the fake canvas for other uses
+            concerned = self.canvas.find_withtag("line")  # what you want
+            for obj in concerned:
+                config = {opt: self.canvas.itemcget(obj, opt) for opt in self.canvas.itemconfig(obj)}
+                config["tags"] = str(obj)  # I can retrieve the ID in "w" later with this trick
+                self.draft.create_line(*self.canvas.coords(obj), **config)
+
+            a = (self.draft.coords(1)[0], self.draft.coords(1)[1], 0)
+            b = (self.draft.coords(1)[2], self.draft.coords(1)[3], 1)
+            c = (self.draft.coords(3)[0], self.draft.coords(3)[1], 2)
+            d = (self.draft.coords(3)[2], self.draft.coords(3)[3], 3)
+            points = [a, b, c, d]
+            # points = sorted(points, key=lambda x: (x[0], x[1]))
+
+            flip = 1
+            #caso verticale
+            if self.selected == 2 and b[0] < a[0]:
+                flip = -1
+            if self.selected == 4 and a[0] > b[0]:
+                flip = -1
+            if self.selected == 1 and a[1] > d[1]:
+                flip = -1
+            if self.selected == 3 and d[1] < a[1]:
+                flip = -1
+
+            p1 = min(points, key=lambda p: p[0])
+            candidati = [item for item in points if item[0] == p1[0]]
+            if len(candidati) > 1:
+                p1 = min(candidati, key=lambda p: p[1])
+            index = p1[2]
+            p1 = (p1[0], p1[1])
+            p2 = points[(index + 1*flip) % 4]
+            p3 = points[(index + 2*flip) % 4]
+            p4 = points[(index + 3*flip) % 4]
+
+            print("index a " + str(index))
+            print(points)
+
+            # p1 = self.point_selector(np.min, np.min)(points)
+            # p2 = self.point_selector(np.min, np.max)(points)
+            # p3 = self.point_selector(np.max, np.max)(points)
+            # p4 = self.point_selector(np.max, np.min)(points)
+
+            self.canvas.coords(self.outside[0], omin_x, omin_y, omin_x, omax_y, omax_x, omax_y, p3[0], p3[1], p4[0],
+                               p4[1], p1[0], p1[1])
+            self.canvas.coords(self.outside[1], omin_x, omin_y, omax_x, omin_y, omax_x, omax_y, p3[0], p3[1], p2[0],
+                               p2[1], p1[0], p1[1])
+
         self._show()
+
+    def update_outside(self):
+        box_image = self.canvas.coords(self.container)  # get image area
+        box_img_int = tuple(map(int, box_image))
+        omin_x, omin_y, omax_x, omax_y = box_img_int
 
     def point_selector(self, func1, func2):
         return lambda a: func1(a[a[:, 0] == func2(a[:, 0])], 0)
@@ -175,8 +237,6 @@ class ShearRectangle:
 
         box_image = self.canvas.coords(self.container)  # get image area
         box_img_int = tuple(map(int, box_image))
-
-        #TODO questa cosa falla in una funzione
         omin_x, omin_y, omax_x, omax_y = box_img_int
 
         up_coord = self._coord_mapping((imin_x, imin_y, imax_x, imax_y), box_img_int)
@@ -189,6 +249,14 @@ class ShearRectangle:
         self.canvas.coords(self.rect[2], imax_x, imax_y, imin_x, imax_y),
         self.canvas.coords(self.rect[3], imin_x, imax_y, imin_x, imin_y)
 
+        a = (imin_x, imin_y)
+        b = (imax_x, imin_y)
+        c = (imax_x, imax_y)
+        d = (imin_x, imax_y)
+        self.canvas.coords(self.outside[0], omin_x, omin_y, omin_x, omax_y, omax_x, omax_y, c[0], c[1], d[0], d[1],
+                           a[0], a[1]),
+        self.canvas.coords(self.outside[1], omin_x, omin_y, omax_x, omin_y, omax_x, omax_y, c[0], c[1], b[0], b[1],
+                           a[0], a[1])
 
         self._show()
 
@@ -231,16 +299,20 @@ class ShearRectangle:
     def _hide(self):
         for r in self.rect:
             self.canvas.itemconfigure(r, state=tk.HIDDEN)
+        for o in self.outside:
+            self.canvas.itemconfigure(o, state=tk.HIDDEN)
 
     def _show(self):
         for r in self.rect:  # Make sure all are now visible.
             self.canvas.itemconfigure(r, state=tk.NORMAL)
+        for o in self.outside:
+            self.canvas.itemconfigure(o, state=tk.NORMAL)
 
     def clear(self, event=None):
         self._hide()
         self.start = (0, 0)
         self.end = (self.width, self.height)
-
+        self.draft = None
     # def crop(self, h_border=0, v_border=0) -> Image.Image:
     #     # left, top, right, bottom = self._get_coords(self.start, self.end)
     #     left, top = self.start
