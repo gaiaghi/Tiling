da04c39105b6574ab17bb4759867d8f92386f624
Gaia Ghidoni <gaia.ghid@gmail.com>
2025-12-11 20:41:35 +0100

    correzioni e modifiche su blur

--- patch_fitting.py
+++ patch_fitting.py
@@ -270,6 +270,7 @@ def patch_fitting(im_src, src_map, im_input, offset, seam_map: SeamMap, error_re
     if use_old_cut: use_grad = False
 
     # background and mask - for blurring
+    h_og, w_og, _ = im_input.shape
     img_bg = np.copy(im_src)
     print(img_bg.shape, im_input.shape)
     img_bg[np.where(src_map == 0)] = im_input[np.where(src_map == 0)]
@@ -315,7 +316,7 @@ def patch_fitting(im_src, src_map, im_input, offset, seam_map: SeamMap, error_re
         Image.fromarray((255 * img_left).astype(np.uint8)).save(OUT_DIR_SUBPATCH+"_left_right_" + str(i) + ".png")
 
     off_x, off_y = offset
-
+    print("°°°°°°°°°°° offset ", off_x, off_y)
     copy_region = Region(error_region.x1, error_region.y1, error_region.x2, error_region.y2)
     copy_region.clip(0, 0, height, width)
     print("copy region: x1, x2 - y1, y2: (", copy_region.x1, ", ", copy_region.x2, ") - (", copy_region.y1, ", ",
@@ -352,17 +353,40 @@ def patch_fitting(im_src, src_map, im_input, offset, seam_map: SeamMap, error_re
                     update_seam_map(G, seam_map, right, left, bottom_nbr, im_src, False)
 
     im3 = Image.fromarray(ud_area.astype(np.uint8))
-    imgg = np.copy(ud_area)
-    #TODO controllo anche sul massimo degli indici--> magari un divieto di selezionare sul bordo?
-    imgg[max(0,error_region.x1-10):error_region.x2+10, max(0,error_region.y1-10):error_region.y2+10] = im_input[
-                                                                               max(0, error_region.x1 - off_x-10):error_region.x1 - off_x + h+10,
-                                                                               max(0, error_region.y1 - off_y-10):error_region.y1 - off_y + w+10]
+    img_patch = np.copy(ud_area)
+
+
+    # if (error_region.x1-10) > 0 and (error_region.x1 - off_x-10 > 0):
+    #     if (error_region.y1-10 > 0) and (error_region.y1 - off_y-10 > 0):
+    #         img_patch[error_region.x1-15:error_region.x2+15, error_region.y1-15:error_region.y2+15] = im_input[
+    #                                                 error_region.x1 - off_x-15:error_region.x1 - off_x + h+15,
+    #                                                 error_region.y1 - off_y-15:error_region.y1 - off_y + w+15]
+
+
+    #TODO numero di pixel in più da compiare varia perchè 15 non è abbastanza per patch di 500 ---> risolvi
+
+    # traslo l'immagine di input dell'offset scelto così estraggo l'immagine patch direttamente con le sue coordinate
+    tran_im_og = ImageChops.offset(Image.fromarray(im_input.astype(np.uint8)), off_x, off_y)
+    # tran_im_og.show()
+    tran_im_og = np.array(tran_im_og, dtype=np.uint8)
+    # img_patch[max(0, error_region.x1 - 5):min(error_region.x2 + 5, h_og),
+    #           max(0, error_region.y1 - 5):min(error_region.y2 + 5, h_og)] = tran_im_og[
+    #                                                                           max(0, error_region.x1 - 5):min(
+    #                                                                               error_region.x2 + 5, h_og),
+    #                                                                           max(0, error_region.y1 - 5):min(
+    #                                                                               error_region.y2 + 5, h_og)]
+
     if blur:
+        # scelto empiricamente
+        blr_value = ((w+h)/2) / 100 + 0.5
+        print("blur value: ", blr_value)
         mask = Image.fromarray(ud_mask.astype(np.uint8), mode="L")
-        blr_mask = mask.filter(ImageFilter.GaussianBlur(5))
+        blr_mask = mask.filter(ImageFilter.GaussianBlur(blr_value))
         img_prev = Image.fromarray(img_bg.astype(np.uint8))
-        img_upd = Image.fromarray(imgg.astype(np.uint8))
-        # blurred = Image.composite(img_upd.convert("RGBA"), img_prev.convert("RGBA"), blr_mask).show()
+        # img_upd = Image.fromarray(img_patch.astype(np.uint8))
+        img_upd = Image.fromarray(tran_im_og.astype(np.uint8))
+        # blr_mask.show()
+        blurred = Image.composite(img_upd.convert("RGBA"), img_prev.convert("RGBA"), blr_mask).show()
         im_src = np.array((Image.composite(img_upd.convert("RGBA"), img_prev.convert("RGBA"), blr_mask)).convert("RGB"))
         im_src[np.where(src_map == 0)] = 0
 
@@ -675,23 +699,29 @@ def get_offset_subpatch_matching(im_src, src_map, im_input, patch_region: Region
     prob_map /= np.sum(prob_map)
 
     #GG tolto randomicità
-    # idx = np.random.choice(idx_map, size=1, p=prob_map.reshape(-1))
-    argmax = np.argmax(prob_map.reshape(-1))
-    # argmax = np.argmax(np.flip(prob_map,0).reshape(-1))
-    argmax = np.argmin(cost_map)
-    # off_x, off_y = np.unravel_index(idx, prob_map.shape)
-    # off_x, off_y = np.unravel_index(idx_map[argmax], cost_map.shape)
+    # argmax = np.argmax(prob_map.reshape(-1))
 
     # off_x, off_y = np.unravel_index(idx_map[argmax], prob_map.shape)
-    #TODO modifica qui
-    xs, ys = np.unravel_index(np.argsort(cost_map, axis=None), cost_map.shape)
-    ii = list(zip(xs[:5], ys[:5]))
-    off_x, off_y = np.unravel_index(cost_map.argmin(), cost_map.shape)
+    # questo era per estrarre i primi 5
+    # xs, ys = np.unravel_index(np.argsort(cost_map, axis=None), cost_map.shape)
+    # ii = list(zip(xs[:5], ys[:5]))
+    # off_x, off_y = random.choice(ii)
 
+    #minimo assoluto (per stampa)
+    off_x, off_y = np.unravel_index(cost_map.argmin(), cost_map.shape)
     print("offset MIN ", off_x, off_y)
+
+    # argmin = np.argmin(cost_map)
+
+    # estrazione dei minimi + scelta random
+    epsilon = 0.15
+    mins_x, mins_y = np.where(cost_map < cost_map.min()*(1+epsilon))
+    mins_coord = list(zip(mins_x, mins_y))
+    off_x, off_y = random.choice(mins_coord)
+
+    print("numero di minimi: ", mins_x, mins_y)
     print("cost value MIN ", cost_map[off_x, off_y])
-    print("altri indici ", ii)
-    off_x, off_y = random.choice(ii)
+    # print("altri indici ", ii)
     print("offset estratto ", off_x, off_y)
     print("cost value ", cost_map[off_x, off_y])
 
